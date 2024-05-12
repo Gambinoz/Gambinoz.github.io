1aee37b3e11ebce429e3f512d48527ee6c309771
Gambinoz <138535797+Gambinoz@users.noreply.github.com>
2024-05-11 23:11:54 -0600

    Update styles.js

--- aglae/styles.js
+++ aglae/styles.js
@@ -1,16 +1,8 @@
 function comprar() {
-    // Obtener el valor de la cantidad de manzanas y calcular el total
     var cantidad = document.getElementById("cantidad").value;
-    var total = cantidad * 25; // Precio por manzana
-
-    // Mostrar el total a pagar
+    var total = cantidad * 25;
     document.getElementById("total").textContent = "$" + total;
-
-    // Esperar un momento antes de mostrar el mensaje de agradecimiento
     setTimeout(function() {
         alert("¡Gracias por comprar!");
-    }, 1000); // 1000 milisegundos = 1 segundo
-
-    // Evitar que el formulario se envíe
-    return false;
-}
+    }, 1000);
+    return false;}
